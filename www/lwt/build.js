5579008ba99f3f6a232ed8286f4a57281695ad5a
Heikki Pora <heikki.pora@reaktor.fi>
2012-05-16 20:04:59 +0300

    Fix build paths

--- www/lwt/build.js
+++ www/lwt/build.js
@@ -2,13 +2,14 @@ load("steal/rhino/rhino.js");
 steal('steal/build').then('steal/build/scripts', 'steal/build/styles', 'steal/build/resources', function () {
     commonSubstitutions = [
         {regexp: /steal\.js\?lwt/mgi, replaceWith: 'steal.production.js?lwt'},
-        {regexp: /<!--placeholder_for_production\.css-->/mgi, replaceWith: '<link rel="stylesheet" href="production.css">'}
+        {regexp: /<!--placeholder_for_production\.css-->/mgi, replaceWith: '<link rel="stylesheet" href="lwt/production.css">'}
     ]
     steal.build('lwt/build.html', {
         to: 'out/lwt',
         resources: [
-            {file: 'lwt/index.html', to: 'index.html', replace: commonSubstitutions},
+            {file: 'lwt/index.html', to: '../index.html', replace: commonSubstitutions},
             {file: 'out/lwt/production.js', to: 'production.js', replace: [
+                {regexp: /images\//mgi, replaceWith: 'lwt/images/'},
                 {regexp: /out\/lwt\/production\.css/mgi, replaceWith: './production.css'}
             ]},
             {file: 'out/lwt/production.css', to: 'production.css', replace: [
