5ca731e7803f0d662d6f29e9a2a0509c34992a2d
Heikki Pora <heikki.pora@reaktor.fi>
2014-08-25 16:19:44 +0300

    Remove old JMVC site

--- old/jquery/i18n/jquery.resourceKey.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $.fn.replaceResourceKeys = function(T) {
-        $("[resourceKey]", this).each(function() {
-            var that = $(this);
-            that.html(T(that.attr("resourceKey")).replace("\n", "<br/>"));
-            that.removeAttr("resourceKey");
-        })
-    }
-})($)
